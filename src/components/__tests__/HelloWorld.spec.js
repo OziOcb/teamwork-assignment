38915875f81d63b11724f6a7338af066b1cac6f5
OziOcb <pozyzniewski@yahoo.com>
2022-06-14 10:15:08 +0100

    Clean up the boilerplate

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,20 @@
 import { describe, it, expect } from "vitest";
 
-import { mount } from "@vue/test-utils";
-import HelloWorld from "../HelloWorld.vue";
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
+
+function factory({ data, propsData } = {}) {
+  return shallowMount(HelloWorld, {
+    data() {
+      return { ...data };
+    },
+    propsData: { ...propsData },
+  });
+}
 
 describe("HelloWorld", () => {
-  it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
+  it("renders HelloWorld component", () => {
+    const wrapper = factory();
+    expect(wrapper.findComponent(HelloWorld).exists()).toBe(true);
   });
 });
