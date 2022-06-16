8a6acff2feb2b3753e529c6993af8e3d33539593
OziOcb <pozyzniewski@yahoo.com>
2022-06-16 21:53:10 +0100

    Add global Sass variables

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <!-- <ThePopup /> -->
+
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
@@ -11,8 +13,36 @@
 
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+// import ThePopup from "@/components/ThePopup.vue";
 </script>
 
 <style lang="scss">
-@import "@/assets/base.css";
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+  position: relative;
+  font-weight: normal;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  min-height: 100vh;
+  color: $color-text-primary;
+  background: $color-body-bg;
+  transition: color $duration-transition-base * 2,
+    background-color $duration-transition-base * 2;
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 1.6rem;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
